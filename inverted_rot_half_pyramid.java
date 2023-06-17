cbc602fbb35138567187f3c5497b5f69bb030b27
Akash Modanwal <akashmodanwal88.am@gmail.com>
2023-06-17 19:34:10 +0530

    pattern file commited

---
+++ inverted_rot_half_pyramid.java
@@ -0,0 +1,15 @@
+public class inverted_rot_half_pyramid{
+    public static void inverted_rot_half_pyra(int n) {
+        for(int i = 1;i<=n;i++){
+            for(int j = 1;j<=n-i;j++){
+                System.out.print(" ");
+            }for(int j = 1;j<=i;j++){
+            System.out.print("*");
+            }
+        System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        inverted_rot_half_pyra(5);
+    }
+}
